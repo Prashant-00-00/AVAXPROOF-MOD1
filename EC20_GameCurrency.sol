4ba376775803c88c99b0e9efbbbb9b3cb18f2ddd
Prashant-00-00 <sharma.prashant.8375@gmail.com>
2024-08-29 21:52:40 +0530

    Code changed

--- EC20_GameCurrency.sol
+++ EC20_GameCurrency.sol
@@ -5,8 +5,8 @@ contract ERC20 {
     uint public totalSupply;
     mapping(address => uint) public balanceOf;
     mapping(address => mapping(address => uint)) public allowance;
-    string public name = "Hooman";
-    string public symbol = "LOVE";
+    string public name = "Prashant";
+    string public symbol = "PRNT";
     uint8 public decimals = 18;
 
     event Transfer(address indexed from, address indexed to, uint value);
